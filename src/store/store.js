e0f74b0b9c3220799860225b3d560f2dfa972f35
tiagon <radek.wojciechowski1997@gmail.com>
2020-05-25 00:12:55 +0200

    2.3

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/reducer';
 
-/* eslint-disable no-underscore-dangle */
-const devTools =
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : null;
-
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), devTools));
-/* eslint-enable */
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
